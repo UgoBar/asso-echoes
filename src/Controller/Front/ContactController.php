4ed10fb7ba41d6d550587778947a0b85b395ed0e
UgoBar <ugo17190@gmail.com>
2024-02-07 09:55:25 +0100

    Try send mail 6

--- src/Controller/Front/ContactController.php
+++ src/Controller/Front/ContactController.php
@@ -7,10 +7,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Mailjet\Client;
@@ -20,12 +17,11 @@ class ContactController extends AbstractController
 {
 
     #[Route('/contact', name: 'front_contact')]
-    public function index(Request $request, MailerInterface $mailer, ValidatorInterface $validator): Response
+    public function index(Request $request, ValidatorInterface $validator): Response
     {
         if ($request->isMethod('POST')) {
             $data = $request->request->all();
-//            $this->sendEmail($mailer, $validator, $data);
-            $this->sendMailViaMailjet($validator, $data);
+            $this->sendEmail($validator, $data);
         }
 
         return $this->render('front/contact.html.twig', [
@@ -83,10 +79,8 @@ class ContactController extends AbstractController
         }
     }
 
-    /**
-     * @throws TransportExceptionInterface
-     */
-    public function sendEmail(MailerInterface $mailer, ValidatorInterface $validator, array $data): RedirectResponse
+
+    public function sendEmail(ValidatorInterface $validator, array $data): RedirectResponse
     {
         // Validate data
         $constraints = new Assert\Collection([
@@ -118,112 +112,19 @@ class ContactController extends AbstractController
             return $this->redirectToRoute('front_contact');
         }
 
-        $email = (new Email())
-            ->from('contact@association-echoes.fr')
-            ->to('ugo17190@gmail.com')
-            ->subject($data['subject'])
-            ->html('<p>'. $data['message'] .'</p>');
-        $mailer->send($email);
+        // Send mail
+        // $mailTo  = 'association.echoes@gmail.com';
+        $mailTo  = 'ugo17190@gmail.com';
 
-//        // Send mail
-//        // $mailTo  = 'association.echoes@gmail.com';
-//        $mailTo  = 'ugo17190@gmail.com';
-//
-//        $headers  = 'MIME-Version: 1.0' . "\r\n";
-//        $headers .= 'From: Association Echoes <contact@association-echoes.fr>'."\r\n";
-//        $headers .= 'Reply-To: '. $data['email']."\r\n";
-//        $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-//        $headers .= 'X-Mailer: PHP/' . phpversion();
-//
-//        mail($mailTo, $data['subject'], $data['message'], $headers);
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'From: Association Echoes <contact@association-echoes.fr>'."\r\n";
+        $headers .= 'Reply-To: '. $data['email']."\r\n";
+        $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+        $headers .= 'X-Mailer: PHP/' . phpversion();
+
+        mail($mailTo, $data['subject'], $data['message'], $headers);
 
         $this->addFlash('success', 'Votre email a bien été envoyé ! Nous vous répondrons dans les plus brefs délais.');
         return $this->redirectToRoute('front_contact');
-    }
-
-    public function sendMailViaMailjet(ValidatorInterface $validator, array $data)
-    {
-        // Validate data
-        $constraints = new Assert\Collection([
-            'name' => [
-                new Assert\NotBlank(null, 'Le nom doit être renseigné'),
-            ],
-            'email' => [
-                new Assert\NotBlank(null, 'L\'email doit être renseigné'),
-                new Assert\Email(null, 'L\'email n\'est pas valide'),
-            ],
-            'subject' => [
-                new Assert\NotBlank(null, 'Le sujet doit être renseigné'),
-            ],
-            'message' => [
-                new Assert\NotBlank(),
-                new Assert\Regex([
-                    'pattern' => '/^[^<>;=#{}%^*()\/]+$/',
-                    'message' => 'Le message ne doit pas contenir de caractères spéciaux.',
-                ]),
-            ],
-        ]);
-
-        $errors = $validator->validate($data, $constraints);
-
-        if (count($errors) > 0) {
-            foreach ($errors as $error) {
-                $this->addFlash('error', $error->getMessage());
-            }
-            return $this->redirectToRoute('front_contact');
-        }
-
-        // Initialisation du client Mailjet
-        $mj = new Client($this->getParameter('app.mailjet_access_key'), $this->getParameter('app.mailjet_secret_key'), true, ['version' => 'v3.1']);
-
-        // Paramètres de l'e-mail
-        $oldBody = [
-            'Messages' => [
-                [
-                    'From' => [
-                        'Email' => $data['email'],
-                        'Name' => $data['name'],
-                    ],
-                    'To' => [
-                        [
-                            'Email' => 'ugo17190@gmail.com',
-                            'Name' => 'Ugo Barathe',
-                        ],
-                    ],
-                    'Subject' => $data['subject'],
-                    'TextPart' => $data['message'],
-                ],
-            ],
-        ];
-
-        $body = [
-            'Messages' => [
-                [
-                    'From' => [
-                        'Email' => "pilot@mailjet.com",
-                        'Name' => "Mailjet Pilot"
-                    ],
-                    'To' => [
-                        [
-                            'Email' => 'ugo17190@gmail.com',
-                            'Name' => 'Ugo Barathe',
-                        ]
-                    ],
-                    'Subject' => "Your email flight plan!",
-                    'TextPart' => "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
-                    'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!"
-                ]
-            ]
-        ];
-
-        // Envoi de l'e-mail
-        $response = $mj->post(Resources::$Email, ['body' => $body]);
-
-        // Traitement de la réponse
-        if ($response->success()) {
-            return $this->redirectToRoute('front_contact');
-        } else {
-            return new Response('Erreur lors de l\'envoi de l\'e-mail: ' . $response->getStatus());
-        }
     }
 }
